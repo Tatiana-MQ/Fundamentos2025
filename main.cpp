2cc1b3e493d713a4dfff226bff004263cc851f91
Tatiana M <tamendezquiros@gmail.com>
2025-06-11 19:22:50 -0600

    Mensaje de bienvenida en Main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
+#include <iomanip>  // Profe, esto lo agregue para alinear columnas con setw, o ordenar bien a la hora de dar aprobados y reporbados, con sus datos
 using namespace std;
 
 int main (){
-
+    cout<<"------------------------------------------------------------\n";
+    cout<< right << setw(40)<< "Bienvenido al programa"<<endl;
+    cout<<"------------------------------------------------------------\n";
 
     return 0;
 }
